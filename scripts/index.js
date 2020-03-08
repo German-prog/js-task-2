320b04f328ba0afe3efa4e29cab48ff535f34249
German-prog <56883343+German-prog@users.noreply.github.com>
2020-03-08 14:02:01 +0200

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+
+const k1 = "5px";
+const k2 = "12djd334";
+const k3 = "12.45asdwe" ;
+const k4 =  "qwqweeewq";
+Number ("5px");
+console.log(Number.parseFloat("5px"));
+console.log(Number.parseFloat("12djd334"));
+console.log(Number.parseFloat("12.45asdwe12"));
+console.log(Number.parseFloat("qwqweeeq"));
